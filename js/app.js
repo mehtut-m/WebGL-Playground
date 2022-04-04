65445209f86878fdbdf347390c5baf57f3ef7e38
mehtut-m <mehtut.m@gmail.com>
2022-04-04 16:52:06 +0700

    creating arc

---
+++ js/app.js
@@ -0,0 +1,71 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import fragment from '../js/shaders/fragment.glsl';
+import vertex from '../js/shaders/vertex.glsl';
+
+export default class Sketch {
+  constructor(options) {
+    this.time = 0;
+    this.container = options.dom;
+    this.scene = new THREE.Scene();
+
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      this.width / this.height,
+      0.01,
+      10
+    );
+    this.camera.position.z = 1;
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(this.width, this.height);
+    this.container.appendChild(this.renderer.domElement);
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    this.resize();
+    this.setUpResize();
+    this.addObjects();
+    this.render();
+  }
+
+  setUpResize() {
+    window.addEventListener('resize', this.resize.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+    this.camera.updateProjectionMatrix();
+  }
+
+  addObjects() {
+    this.geometry = new THREE.PlaneBufferGeometry(0.5, 0.5, 50, 50);
+    this.material = new THREE.MeshNormalMaterial();
+
+    this.material = new THREE.ShaderMaterial({
+      side: THREE.DoubleSide,
+      fragmentShader: fragment,
+      vertexShader: vertex,
+      wireframe: true,
+    });
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+  }
+
+  render() {
+    this.time += 0.05;
+    this.mesh.rotation.x = this.time / 2000;
+    this.mesh.rotation.y = this.time / 1000;
+    this.renderer.render(this.scene, this.camera);
+    window.requestAnimationFrame(this.render.bind(this));
+  }
+}
+
+new Sketch({ dom: document.getElementById('container') });
